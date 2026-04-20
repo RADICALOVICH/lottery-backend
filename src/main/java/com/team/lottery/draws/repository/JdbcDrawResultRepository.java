f8f207ff4198b0740f074bc00388b5b44efaf532
kurindinvv <kurindinvv@ERV-W0013>
2026-04-20 14:02:37 +0300

    some refactoring for draw repository

--- src/main/java/com/team/lottery/draws/repository/JdbcDrawResultRepository.java
+++ src/main/java/com/team/lottery/draws/repository/JdbcDrawResultRepository.java
@@ -1,13 +1,18 @@
 package com.team.lottery.draws.repository;
 
-import com.team.lottery.common.db.JdbcHelper;
 import com.team.lottery.draws.model.DrawResult;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Types;
 import java.time.OffsetDateTime;
 import java.util.Optional;
 
 public class JdbcDrawResultRepository implements DrawResultRepository {
+
     private final DataSource ds;
 
     public JdbcDrawResultRepository(DataSource ds) {
@@ -16,63 +21,71 @@ public class JdbcDrawResultRepository implements DrawResultRepository {
 
     @Override
     public DrawResult save(DrawResult drawResult) {
-        return JdbcHelper.withConnection(ds, c -> {
-            var inserted = JdbcHelper.query(
-                    c,
-                    "INSERT INTO draw_results (draw_id, winning_ticket_id, drawn_at) " +
-                            "VALUES (?, ?, ?) " +
-                            "RETURNING id, draw_id, winning_ticket_id, drawn_at",
-                    ps -> {
-                        ps.setLong(1, drawResult.getDrawId());
-                        if (drawResult.getWinningTicketId() != null) {
-                            ps.setLong(2, drawResult.getWinningTicketId());
-                        } else {
-                            ps.setNull(2, java.sql.Types.BIGINT);
-                        }
-                        ps.setObject(3, drawResult.getDrawnAt());
-                    },
-                    rs -> {
-                        DrawResult saved = new DrawResult();
-                        saved.setId(rs.getLong("id"));
-                        saved.setDrawId(rs.getLong("draw_id"));
+        String sql = """
+                INSERT INTO draw_results (draw_id, winning_ticket_id, drawn_at)
+                VALUES (?, ?, ?)
+                RETURNING id, draw_id, winning_ticket_id, drawn_at
+                """;
 
-                        long winningTicketId = rs.getLong("winning_ticket_id");
-                        if (!rs.wasNull()) {
-                            saved.setWinningTicketId(winningTicketId);
-                        }
+        try (Connection connection = ds.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
 
-                        saved.setDrawnAt(rs.getObject("drawn_at", OffsetDateTime.class));
-                        return saved;
-                    }
-            );
+            statement.setLong(1, drawResult.getDrawId());
 
-            return inserted.get(0);
-        });
+            if (drawResult.getWinningTicketId() != null) {
+                statement.setLong(2, drawResult.getWinningTicketId());
+            } else {
+                statement.setNull(2, Types.BIGINT);
+            }
+
+            statement.setObject(3, drawResult.getDrawnAt());
+
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    return mapDrawResult(resultSet);
+                }
+                throw new SQLException("Failed to insert draw result");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Database error while saving draw result", e);
+        }
     }
 
     @Override
     public Optional<DrawResult> findByDrawId(Long drawId) {
-        return JdbcHelper.withConnection(ds, c -> {
-            var rows = JdbcHelper.query(
-                    c,
-                    "SELECT id, draw_id, winning_ticket_id, drawn_at " +
-                            "FROM draw_results WHERE draw_id = ?",
-                    ps -> ps.setLong(1, drawId),
-                    rs -> {
-                        DrawResult drawResult = new DrawResult();
-                        drawResult.setId(rs.getLong("id"));
-                        drawResult.setDrawId(rs.getLong("draw_id"));
+        String sql = """
+                SELECT id, draw_id, winning_ticket_id, drawn_at
+                FROM draw_results
+                WHERE draw_id = ?
+                """;
 
-                        long winningTicketId = rs.getLong("winning_ticket_id");
-                        if (!rs.wasNull()) {
-                            drawResult.setWinningTicketId(winningTicketId);
-                        }
+        try (Connection connection = ds.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
 
-                        drawResult.setDrawnAt(rs.getObject("drawn_at", OffsetDateTime.class));
-                        return drawResult;
-                    }
-            );
-            return rows.stream().findFirst();
-        });
+            statement.setLong(1, drawId);
+
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    return Optional.of(mapDrawResult(resultSet));
+                }
+                return Optional.empty();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Database error while finding draw result by draw id", e);
+        }
+    }
+
+    private DrawResult mapDrawResult(ResultSet rs) throws SQLException {
+        DrawResult drawResult = new DrawResult();
+        drawResult.setId(rs.getLong("id"));
+        drawResult.setDrawId(rs.getLong("draw_id"));
+
+        long winningTicketId = rs.getLong("winning_ticket_id");
+        if (!rs.wasNull()) {
+            drawResult.setWinningTicketId(winningTicketId);
+        }
+
+        drawResult.setDrawnAt(rs.getObject("drawn_at", OffsetDateTime.class));
+        return drawResult;
     }
 }
